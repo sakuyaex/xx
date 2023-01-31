be2ea193635f238faec26156ca95a709a8accab5
KINAKA <96835704+sakuyaex@users.noreply.github.com>
2023-01-31 22:03:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ls - sudo -i
+echo "sudo -i"
+echo "apt update -y "
+echo "apt install -y curl"
+echo "apt install -y socat"
+echo "curl https://get.acme.sh | sh"
+echo "~/.acme.sh/acme.sh --register-account -m kanontte@gmail.com"
+echo "~/.acme.sh/acme.sh  --issue -d zx.suzukiex.top   --standalone"
+echo "~/.acme.sh/acme.sh --installcert -d zx.suzukiex.top --key-file /root/private.key --fullchain-file /root/cert.crt"
