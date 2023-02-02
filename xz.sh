f65859c3240fb42a56f0940cf9ffb2eaa94ff622
KINAKA <96835704+sakuyaex@users.noreply.github.com>
2023-02-02 22:11:01 +0800

    Update and rename install.sh to xz.sh

---
+++ xz.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update -y 
+apt install -y curl 
+apt install -y socat
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh --register-account -m kanontte@gmail.com
+~/.acme.sh/acme.sh  --issue -d zx.suzukiex.top   --standalone
+~/.acme.sh/acme.sh --installcert -d zx.suzukiex.top --key-file /root/private.key --fullchain-file /root/cert.crt
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
