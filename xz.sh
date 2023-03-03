388f6d11b2126594f6db55097d487adbf1f9badc
KINAKA <96835704+sakuyaex@users.noreply.github.com>
2023-03-03 20:17:25 +0800

    Update xz.sh

--- xz.sh
+++ xz.sh
@@ -2,5 +2,5 @@
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --register-account -m sayakaextra@gmail.com
 ~/.acme.sh/acme.sh  --issue -d zx.sakuyax.top   --standalone
-~/.acme.sh/acme.sh --installcert -d zx.zx.sakuyax.top  --key-file /root/private.key --fullchain-file /root/cert.crt
+~/.acme.sh/acme.sh --installcert -d zx.sakuyax.top  --key-file /root/private.key --fullchain-file /root/cert.crt
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
