047551e85c8954e2ee3b66cbe2f721984ddc4f2a
KINAKA <96835704+sakuyaex@users.noreply.github.com>
2023-03-03 21:04:03 +0800

    Update and rename xz.sh to 01.sh

--- 01.sh
+++ 01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --register-account -m sayakaextra@gmail.com
-~/.acme.sh/acme.sh  --issue -d zx.suzukiex.top   --standalone
-~/.acme.sh/acme.sh --installcert -d zx.suzukiex.top  --key-file /root/private.key --fullchain-file /root/cert.crt
+~/.acme.sh/acme.sh  --issue -d zx.exkinaka.xyz   --standalone
+~/.acme.sh/acme.sh --installcert -d zx.exkinaka.xyz  --key-file /root/private.key --fullchain-file /root/cert.crt
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
